3397ac808cc4864f889f9c143d51acd6740cc402
Pier3150 <158572251+pier3100@users.noreply.github.com>
2025-04-23 15:16:35 +0200

    modified looping functionality using DynamicTask

--- superColliderGitBatch.bat
+++ superColliderGitBatch.bat
@@ -3,16 +3,22 @@ setlocal EnableDelayedExpansion
 
 set /p commitMessage=Enter commit message: 
 
+echo.
+echo === Pushing to SUDJ-peripheral===
 cd /d C:\Users\piert\MakingMusic\SuperCollider
 git add .
 git commit -m "!commitMessage!"
 git push
 
+echo.
+echo === Pushing to SUDJ-execute ===
 cd /d C:\Users\piert\MakingMusic\SuperCollider\SUDJ-execute
 git add .
 git commit -m "!commitMessage!"
 git push
 
+echo.
+echo === Pushing to SUDJ-classes ===
 cd /d C:\ProgramData\SuperCollider\Extensions\SUDJ-classes
 git add .
 git commit -m "!commitMessage!"
