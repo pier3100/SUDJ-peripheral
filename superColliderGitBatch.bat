9d64be1589495bb2b2e717f92326fa91891d72ec
Pier3150 <158572251+pier3100@users.noreply.github.com>
2025-05-11 15:22:02 +0200

    serveral playback/looping/beatjumping bug fixes

--- superColliderGitBatch.bat
+++ superColliderGitBatch.bat
@@ -7,21 +7,21 @@ echo.
 echo === Pushing to SUDJ-peripheral===
 cd /d C:\Users\piert\MakingMusic\SuperCollider
 git add .
-git commit -m "!commitMessage!"
+git commit -am "!commitMessage!"
 git push
 
 echo.
 echo === Pushing to SUDJ-execute ===
 cd /d C:\Users\piert\MakingMusic\SuperCollider\SUDJ-execute
 git add .
-git commit -m "!commitMessage!"
+git commit -am "!commitMessage!"
 git push
 
 echo.
 echo === Pushing to SUDJ-classes ===
 cd /d C:\ProgramData\SuperCollider\Extensions\SUDJ-classes
 git add .
-git commit -m "!commitMessage!"
+git commit -am "!commitMessage!"
 git push
 
 echo.
